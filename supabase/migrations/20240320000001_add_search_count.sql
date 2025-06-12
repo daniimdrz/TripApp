9cabc1220e05daf5ae90265780732cb288fa33ec
daniimdrz <danisanchez1402@gmail.com>
2025-06-12 14:06:14 +0200

    commit

--- supabase/migrations/20240320000001_add_search_count.sql
+++
@@ -1,8 +0,0 @@
--- Add search_count column
-ALTER TABLE mapbox_usage 
-ADD COLUMN IF NOT EXISTS search_count INTEGER NOT NULL DEFAULT 0;
-
--- Update existing record to have search_count = 0
-UPDATE mapbox_usage 
-SET search_count = 0 
-WHERE search_count IS NULL; 
